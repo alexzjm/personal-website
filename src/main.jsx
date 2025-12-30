93f9085aa2b4801357af973280d69ba0b84db32d
Alex Zhang <alexzhangxxxx@gmail.com>
2025-12-29 20:11:28 -0500

    implemented smooth scrolling using lenis

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App.jsx'
+import SmoothScroll from './SmoothScroll.jsx';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SmoothScroll>
+      <App />
+    </SmoothScroll>
   </StrictMode>,
 )
